cd9b10166f3856658c8e5c78385f5d1ce30d36ef
methylDragon <methylDragon@google.com>
2024-08-01 14:04:52 -0700

    [DO NOT MERGE] Test pubsub
    Signed-off-by: methylDragon <methylDragon@intrinsic.ai>

--- intrinsic/platform/pubsub/zenoh_util/zenoh_handle_test.cc
+++ intrinsic/platform/pubsub/zenoh_util/zenoh_handle_test.cc
@@ -1,10 +1,54 @@
 // Copyright 2023 Intrinsic Innovation LLC
 
 #include "intrinsic/platform/pubsub/zenoh_util/zenoh_handle.h"
+#include "intrinsic/platform/pubsub/zenoh_util/zenoh_config.h"
 
+#include "absl/log/log.h"
 #include <gtest/gtest.h>
 
 namespace intrinsic {
+
+void nested_callback(const char *, const void *, const size_t, void *) {
+  LOG(INFO) << "nested_callback";
+}
+
+void test_callback(const char *, const void *, const size_t, void *context) {
+  LOG(INFO) << "test_callback";
+  ZenohHandle *h = reinterpret_cast<ZenohHandle *>(context);
+
+  // if the mutex locks inside the imw implementation are overly protective,
+  // this call will hang as the call stack becomes publish-callback-publish
+  h->imw_publish("nested", "bar", 4);
+}
+
+TEST(ZenohHandleTest, Initialize) {
+  std::string config = GetZenohPeerConfig();
+  std::unique_ptr<ZenohHandle> h(ZenohHandle::CreateZenohHandle());
+
+  EXPECT_NE(h, nullptr);
+  EXPECT_EQ(IMW_OK, h->imw_init(config.c_str()));
+
+  EXPECT_EQ(IMW_OK, h->imw_create_publisher("test", "{}"));
+  EXPECT_EQ(IMW_OK,
+            h->imw_create_subscription("test", test_callback, "{}", h.get()));
+
+  EXPECT_EQ(IMW_OK, h->imw_create_publisher("nested", "{}"));
+  EXPECT_EQ(IMW_OK, h->imw_create_subscription("nested", nested_callback, "{}",
+                                               nullptr));
+
+  // so long as this doesn't deadlock, we'll consider this test "passed"
+  EXPECT_EQ(IMW_OK, h->imw_publish("test", "foo", 4));
+
+  EXPECT_EQ(IMW_OK,
+            h->imw_destroy_subscription("nested", nested_callback, nullptr));
+  EXPECT_EQ(IMW_OK, h->imw_destroy_publisher("nested"));
+
+  EXPECT_EQ(IMW_OK,
+            h->imw_destroy_subscription("test", test_callback, h.get()));
+  EXPECT_EQ(IMW_OK, h->imw_destroy_publisher("test"));
+
+  EXPECT_EQ(IMW_OK, h->imw_fini());
+}
 
 TEST(ZenohHandleTest, AddTopicPrefix) {
   EXPECT_EQ(*ZenohHandle::add_topic_prefix("foo"), "in/foo");
